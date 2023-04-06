1f5e5a336b923f4253aeba1d0e1afad0b68b0d92
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-04-06 16:26:56 +0300

    test kısmı kaldı.

--- kullanici-katilimi/src/components/Form.js
+++ kullanici-katilimi/src/components/Form.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import * as yup from "yup";
+import RegisteredUser from "./RegisteredUser";
 
 const formSchema = yup.object().shape({
   name: yup
@@ -37,60 +38,120 @@ export default function Form() {
   });
 
   const [registeredUser, setRegisteredUser] = useState([]);
-
+  const [buttonDisabledMi, setButtonDisabledMi] = useState(true);
+  const [errors, setErrors] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    terms: "",
+  });
+  const handleReset = () => {
+    setUser({
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+      terms: "",
+    });
+    setErrors({
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+      terms: "",
+    });
+  };
+  const checkFormErrors = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(() => {
+        setErrors({
+          ...errors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          [name]: err.errors[0],
+        });
+      });
+  };
   function handleChange(event) {
-    const { checked, name, value, type } = event.target;
+    const { name, value, type, checked } = event.target;
     let newValue = type === "checkbox" ? checked : value;
-
     setUser({ ...user, [name]: newValue });
+    checkFormErrors(name, newValue);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     axios
       .post("https://reqres.in/api/users", user)
-      .then((response) => setRegisteredUser(response.data))
+      .then((response) => setRegisteredUser([...registeredUser, response.data]))
       .catch((error) => console.log("Error!", error));
   }
 
+  useEffect(() => {
+    formSchema.isValid(user).then((response) => setButtonDisabledMi(!response));
+  }, [user]);
+
   return (
     <div className="FormContainer">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} buttonDisabledMi={buttonDisabledMi}>
         <label>
           Name:
           <input
             type="text"
             name="name"
+            placeholder="Name"
             value={user.name}
             onChange={handleChange}
           />
+          {errors.name !== "" && (
+            <div className="field-error">{errors.name}</div>
+          )}
         </label>
         <label>
           Surname:
           <input
             type="text"
             name="surname"
+            placeholder="Surname"
             value={user.surname}
             onChange={handleChange}
           />
+          {errors.surname !== "" && (
+            <div className="field-error">{errors.surname}</div>
+          )}
         </label>
         <label>
           Email:
           <input
             type="email"
             name="email"
+            placeholder="example@example.com"
             value={user.email}
             onChange={handleChange}
           />
+          {errors.email !== "" && (
+            <div className="field-error">{errors.email}</div>
+          )}
         </label>
         <label>
           Password:
           <input
             type="password"
             name="password"
+            placeholder="Password"
             value={user.password}
             onChange={handleChange}
           />
+          {errors.password !== "" && (
+            <div className="field-error">{errors.password}</div>
+          )}
         </label>
         <label>
           <input
@@ -99,11 +160,22 @@ export default function Form() {
             value={user.terms}
             onChange={handleChange}
             checked={user.terms}
+            class="larger"
           />
           I agree to the terms and conditions as set out by the user agreement.
         </label>
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          disabled={buttonDisabledMi}
+        >
+          SUBMIT
+        </button>
+        <button type="reset" onClick={handleReset}>
+          CLEAN
+        </button>
       </form>
-      <button type="submit">SUBMIT</button>
+      <RegisteredUser registeredUser={registeredUser} />
     </div>
   );
 }
